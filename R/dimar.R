199aeb9761fa7be27877eabeaa6f12f8b69b052c
JanineEgert <egert@imbi.uni-freiburg.de>
2021-03-31 16:54:41 +0200

    input file format and matrix preprocessing

--- R/dimar.R
+++ R/dimar.R
@@ -1,9 +1,22 @@
-dimar <- function(mtx) {
+dimar <- function(mtx,pattern<-NULL) {
   
 if (is.character(mtx)) {
-  filename <- mtx
-  mtx <- dimarReadInMaxQuant(mtx)
+  file <- mtx
+  ext <- strsplit(basename(file), split="\\.")[[1]][-1]
+  if (ext=='tsv'){
+    mtx <- read.table(file='metabolomics.tsv',header=T)
+  } else if (ext=='csv'){
+    mtx <- read.csv(filename, allowEscapes = TRUE, check.names = FALSE,sep = "\t")
+  }
 } else { filename <- {} }
+if (class(mtx) == 'SingleCellExperiment' || class(mtx) == 'SummarizedExperiment') {
+  mtx <- as.matrix(assay(mtx))
+}
+if (is.character(pattern)) {
+  mtx <- as.matrix(mtx[, grepl(pattern, names(mtx))])
+}
+
+mtx <- dimarFiltering(mtx)
 
 coef <- dimarLearnPattern(mtx)
 ref <- dimarConstructReferenceData(mtx)
@@ -13,6 +26,6 @@ Imputations <- dimarDoImputations(sim, c('impSeqRob', 'ppca', 'imputePCA'))
 Performance <- dimarEvaluatePerformance(Imputations, ref, sim, 'RMSE', TRUE)
 Imp <- dimarDoOptimalImputation(mtx, rownames(Performance))
 
-write.csv(Imp, file=paste0("Imp_", filename))
+write.csv(Imp, file=paste0("Imp_", file))
 return(Imp)
 }
